f975c0d4944e5d1943c1badb94390568e92675a5
divyamsingh007 <legendprice007@gmail.com>
2025-02-02 02:45:44 +0530

    Push to practicegit add .

--- HOME_page/js-boilerplate.js
+++ HOME_page/js-boilerplate.js
@@ -2,25 +2,46 @@ let knowMoreBtn = document.getElementById("know-more-btn");
 
 let nav_01 = document.getElementById("nav-01");
 let about_me_sec = document.getElementById("about-me");
+
 let nav_02 = document.getElementById("nav-02");
 let education_sec = document.getElementById("education");
-let nav_03 = document.getElementById("nav-03");
-let nav_04 = document.getElementById("nav-04");
-let nav_05 = document.getElementById("nav-05");
 
-var rect = about_me_sec.getBoundingClientRect();
-console.log(rect);
+let nav_03 = document.getElementById("nav-03");
+let experiences_sec = document.getElementById("experiences");
+
+let nav_04 = document.getElementById("nav-04");
+let skills_sec = document.getElementById("skills");
+
+let nav_05 = document.getElementById("nav-05");
+let interest_sec = document.getElementById("interests");
+
+// to extract DOM values for about-me section
+var info_01 = about_me_sec.getBoundingClientRect().y; //distance from viewport top
+var info_02 = about_me_sec.getBoundingClientRect().height; //element's itself height
+console.log(info_01);
+console.log(info_01 +info_02);
 
 
 window.addEventListener("scroll", function () {
   var hasFired = false;
 
-  if (document.body.scrollTop >= 752 && !hasFired) {
+  // if (info_01 = 0 && !hasFired) {
+  //   hasFired = true;
+  //   nav_01.classList.add("white-underline");
+  //   console.log("Hello Up");
+    
+  // }
+  if (document.body.scrollTop >= info_01 && !hasFired) {
     hasFired = true;
     nav_01.classList.add("white-underline");
+    console.log("Hello Up");
+    
   }
 
-  if (document.body.scrollTop < 250 && hasFired) {
+  if (document.body.scrollTop < (info_01 + info_02) && hasFired) {
     hasFired = false;
+    nav_01.classList.remove("white-underline");
+    console.log("Hello down");
+    
   }
 });
